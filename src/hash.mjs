1a2847d3e1b96203e59a73efeeebfa0cc53857c2
Jascha <jascha@jaeh.at>
2020-01-14 21:13:45 +0100

    hash: add more error handling

--- src/hash.mjs
+++ src/hash.mjs
@@ -4,12 +4,24 @@ import { is, error } from './lib/index.mjs'
 
 const libName = '@webboot/crypto.hashes'
 
-export const create = (data, algorithm = 'sha384') => {
-  if (!is.string(data) || is.empty(data)) {
-    throw error(`${libName}.create: data was not a string or empty. ${typeof data}`, 'E_DATA_EMPTY')
+export const create = (data, options = {}) => {
+  const { algorithm = 'sha384', length = 24 } = options
+
+  if (is.empty(data)) {
+    throw error(`${libName}.create: data can not be empty.`, 'E_DATA_EMPTY')
   }
 
-  const hashIt = crypto.createHash(algorithm)
+  if (!is.string(data) && !is.buffer(data)) {
+    throw error(`${libName}.create: data must be a string or buffer.`, 'E_DATA_TYPE')
+  }
+
+  const opts = {}
+
+  if (algorithm === 'shake256') {
+    opts.outputLength = length
+  }
+
+  const hashIt = crypto.createHash(algorithm, opts)
 
   hashIt.update(data)
 
@@ -21,12 +33,22 @@ export const create = (data, algorithm = 'sha384') => {
   }
 }
 
-export const check = (data, hash, algorithm = 'sha384') => {
-  if (!is.string(data) || is.empty(data)) {
-    throw error(`${libName}.check: data was not a string or empty.`, 'E_DATA_EMPTY')
+export const check = (data, hash, options = {}) => {
+  const { algorithm = 'sha384' } = options
+
+  if (!is.string(data)) {
+    throw error(`${libName}.check: data was not a string.`, 'E_DATA_TYPE')
   }
 
-  if (!is.string(hash) || is.empty(hash)) {
+  if (is.empty(data)) {
+    throw error(`${libName}.check: data can not be empty.`, 'E_DATA_EMPTY')
+  }
+
+  if (!is.string(hash)) {
+    throw error(`${libName}.check: hash was not a string or empty.`, 'E_HASH_TYPE')
+  }
+
+  if (is.empty(hash)) {
     throw error(`${libName}.check: hash was not a string or empty.`, 'E_HASH_EMPTY')
   }
 
