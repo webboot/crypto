c1f34c232b9add4fba60ecdcb73ef003bf6c1afa
Jascha <jascha@jaeh.at>
2020-01-15 00:49:54 +0100

    decrypt/encrypt: uncomment unused authentication functionality, update error handling

--- src/ecdh/cipher/decrypt.mjs
+++ src/ecdh/cipher/decrypt.mjs
@@ -6,8 +6,16 @@ import hash from '../../hash.mjs'
 
 const libName = `@webboot/crypto.ecdh.cipher.decrypt`
 
-export const decrypt = (props = {}) => {
+export const decrypt = props => {
+  if (is.empty(props)) {
+    throw error(
+      `${libName}: function arguments have to be an object and be non-empty`,
+      'E_PROPS_EMPTY',
+    )
+  }
+
   const { aad, nonce, tag, ciphertext } = props
+
   let { secret, algorithm = 'aes-256-cbc' } = props
 
   if (!is.buffer(ciphertext)) {
@@ -18,20 +26,20 @@ export const decrypt = (props = {}) => {
     throw error(`${libName}: ciphertext is empty.`, 'E_CIPHERTEXT_EMPTY')
   }
 
-  if (!is.buffer(secret)) {
-    throw error(`${libName}: secret is not a buffer.`, 'E_CIPHERTEXT_TYPE')
-  }
-
   if (is.empty(secret)) {
     throw error(`${libName}: secret is empty`, 'E_SECRET_EMPTY')
   }
 
-  if (!is.buffer(nonce)) {
-    throw error(`${libName}: nonce is not a buffer.`, 'E_CIPHERTEXT_TYPE')
+  if (!is.buffer(secret)) {
+    throw error(`${libName}: secret is not a buffer.`, 'E_SECRET_TYPE')
   }
 
   if (is.empty(nonce)) {
     throw error(`${libName}: nonce is empty`, 'E_NONCE_EMPTY')
+  }
+
+  if (!is.buffer(nonce)) {
+    throw error(`${libName}: nonce is not a buffer.`, 'E_NONCE_TYPE')
   }
 
   if (secret.length !== '32') {
@@ -40,26 +48,25 @@ export const decrypt = (props = {}) => {
   }
 
   let options = {}
-  if (!is.empty(tag) && !is.empty(aad)) {
-    algorithm = 'aes-256-ccm'
-    options.authTagLength = 16
-  }
+
+  // if (!is.empty(tag) && !is.empty(aad)) {
+  //   algorithm = 'aes-256-ccm'
+  //   options.authTagLength = 16
+  // }
 
   const decipher = crypto.createDecipheriv(algorithm, secret, nonce, options)
 
-  if (!is.empty(tag) && !is.empty(aad)) {
-    decipher.setAuthTag(tag)
+  // if (!is.empty(tag) && !is.empty(aad)) {
+  //   decipher.setAuthTag(tag)
 
-    decipher.setAAD(aad, {
-      plaintextLength: ciphertext.length,
-    })
-  }
+  //   decipher.setAAD(aad, {
+  //     plaintextLength: ciphertext.length,
+  //   })
+  // }
 
-  const deciphered = decipher.update(ciphertext)
+  let result = decipher.update(ciphertext, null, 'utf8')
 
-  const final = decipher.final()
-
-  const result = Buffer.concat([deciphered, final])
+  result += decipher.final('utf8')
 
   return result
 }
--- src/ecdh/cipher/encrypt.mjs
+++ src/ecdh/cipher/encrypt.mjs
@@ -6,18 +6,26 @@ import hash from '../../hash.mjs'
 
 const libName = '@webboot/crypto.ecdh.'
 
-export const encrypt = (props = {}) => {
+export const encrypt = props => {
+  if (is.empty(props)) {
+    throw error(`${libName}encrypt: props has to be non empty`, 'E_PROPS_EMPTY')
+  }
+
+  if (!is.object(props)) {
+    throw error(`${libName}encrypt: props has to be an Object`, 'E_PROPS_TYPE')
+  }
+
   const { aad = false, algorithm = 'aes-256-cbc', data } = props
   let { secret = '' } = props
 
-  if (!is.string(secret) && !is.buffer(secret)) {
-    throw error(`${libName}encrypt: need props.secret to be a string or buffer`, 'E_SECRET_TYPE')
+  if (is.empty(secret)) {
+    throw error(`${libName}encrypt: props.secret has to be non-empty.`, 'E_SECRET_EMPTY')
   }
 
-  if (is.empty(secret)) {
+  if (!is.string(secret) && !is.buffer(secret)) {
     throw error(
-      `${libName}encrypt: need props.secret to be non-empty`,
-      'E_SECRET_EMPTY',
+      `${libName}encrypt: props.secret has to be a buffer or string. ${typeof secret}`,
+      'E_SECRET_TYPE',
     )
   }
 
@@ -27,28 +35,29 @@ export const encrypt = (props = {}) => {
   }
 
   if (is.empty(data)) {
-    throw error(
-      `${libName}encrypt: need props.data to be a non-empty string or buffer.`,
-      'E_DATA_EMPTY',
-    )
+    throw error(`${libName}encrypt: props.data has be non-empty.`, 'E_DATA_EMPTY')
+  }
+
+  if (!is.string(data) && !is.buffer(data)) {
+    throw error(`${libName}encrypt: props.data has to be a string or buffer`, 'E_DATA_TYPE')
   }
 
   const options = {}
 
-  if (!is.empty(aad)) {
-    algorithm = 'aes-256-ccm'
-    options.authTagLength = 16
-  }
+  // if (!is.empty(aad)) {
+  //   algorithm = 'aes-256-ccm'
+  //   options.authTagLength = 16
+  // }
 
   const nonce = crypto.randomBytes(16)
 
   const cipher = crypto.createCipheriv(algorithm, secret, nonce, options)
 
-  if (aad) {
-    cipher.setAAD(aad, {
-      plaintextLength: Buffer.byteLength(data),
-    })
-  }
+  // if (!is.empty(aad)) {
+  //   cipher.setAAD(aad, {
+  //     plaintextLength: Buffer.byteLength(data),
+  //   })
+  // }
 
   const ciphered = cipher.update(data)
 
@@ -62,9 +71,9 @@ export const encrypt = (props = {}) => {
     nonce,
   }
 
-  if (aad) {
-    result.tag = cipher.getAuthTag()
-  }
+  // if (!is.empty(aad)) {
+  //   result.tag = cipher.getAuthTag()
+  // }
 
   return result
 }
