08cceb81357eaa5610684ecd86bddc4ff00d4cfe
Jascha <jascha@jaeh.at>
2020-01-19 19:20:00 +0100

    ecdh/cipher: remove redundant exports

--- src/ecdh/cipher/index.mjs
+++ src/ecdh/cipher/index.mjs
@@ -3,10 +3,3 @@ import encryptor from './encrypt.mjs'
 
 export const decrypt = decryptor
 export const encrypt = encryptor
-
-export const cipher = {
-  decrypt,
-  encrypt,
-}
-
-export default cipher
