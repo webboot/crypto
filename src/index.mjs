7a9f2bb1cc86273ef743c046d57fda613824e706
Jascha <jascha@jaeh.at>
2020-01-21 05:04:41 +0100

    src/index: export gpg

--- src/index.mjs
+++ src/index.mjs
@@ -1,10 +1,15 @@
-import sha from './hash.mjs'
 import diffie from './ecdh/index.mjs'
+import * as pgpgp from './gpg.mjs'
+import hasher from './hash.mjs'
 
-export const hash = sha
+export const hash = hasher
 export const ecdh = diffie
+export const gpg = pgpgp
+export const pgp = pgpgp
 
 export default {
   hash,
   ecdh,
+  gpg,
+  pgp,
 }
