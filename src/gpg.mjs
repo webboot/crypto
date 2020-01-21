0cd6946def3e0a3827ff414195774a3db2ee603a
Jascha <jascha@jaeh.at>
2020-01-21 06:10:38 +0100

    gpg: add parseKeys for --list-keys output

--- src/gpg.mjs
+++ src/gpg.mjs
@@ -4,11 +4,10 @@ import { error } from './lib/error.mjs'
 
 const libName = '@webboot/crypto.lib.exec'
 
-// this always resolves, errors get handled if they get returned by awaited functions,
-// not thrown
-
+// this always resolves.
+// errors get handled if they get returned by awaited functions.
 export const gpg = (cmd = '--list-keys', options = {}) =>
-  new Promise((resolve, reject) => {
+  new Promise(resolve => {
     if (!cmd.startsWith('gpg')) {
       cmd = `gpg ${cmd}`
     }
@@ -27,6 +26,51 @@ export const gpg = (cmd = '--list-keys', options = {}) =>
       resolve(result)
     })
   })
+
+gpg.parseKeys = string => {
+  const keys = {}
+  let currentKey = ''
+
+  console.log({ string })
+
+  string.split('\n').map((line, i, lines) => {
+    line = line.trim()
+    if (!line) {
+      return
+    }
+
+    if (line.startsWith('pub')) {
+      // new key
+      currentKey = lines[i + 1].trim()
+
+      const [_, algorithm, date, cap, _1, expires] = line.split(' ').filter(a => a)
+
+      keys[currentKey] = {
+        algorithm,
+        cap,
+        date,
+        expires: expires && expires.replace(']', ''),
+        key: currentKey,
+        sub: [],
+        users: [],
+      }
+    } else if (line.startsWith('uid')) {
+      const [cap, rest] = line
+        .replace('uid', '')
+        .trim()
+        .split(']')
+        .filter(a => a)
+      const [name, email] = rest.split(' <')
+      keys[currentKey].users.push({
+        name: name.trim(),
+        cap: cap.replace('[', '').trim(),
+        email: email.replace('>', ''),
+      })
+    }
+  })
+
+  return keys
+}
 
 export const pgp = gpg
 
