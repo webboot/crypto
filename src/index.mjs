c2cf3975a4e8d3a263274c0139e260d0f650718e
jascha <jascha@jaeh.at>
2020-01-26 07:13:43 +0100

    add wordlist

--- src/index.mjs
+++ src/index.mjs
@@ -1,15 +1,18 @@
 import diffie from './ecdh/index.mjs'
 import pgpgp from './gpg.mjs'
 import hasher from './hash.mjs'
+import wordList from './wordlist.mjs'
 
 export const hash = hasher
 export const ecdh = diffie
 export const gpg = pgpgp
 export const pgp = pgpgp
+export const wordlist = wordList
 
 export default {
   hash,
   ecdh,
   gpg,
   pgp,
+  wordlist,
 }
