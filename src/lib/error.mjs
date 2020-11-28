f423bc5603477d2e0d5da045adf5e2ff686ce0c2
jascha <jascha@jaeh.at>
2020-11-28 20:56:42 +0100

    lib/error: accept strings or errors as msg

--- src/lib/error.mjs
+++ src/lib/error.mjs
@@ -1,17 +1,18 @@
-import { is } from './is.mjs'
-
 export const error = (msg, name = 'E_UNKNOWN') => {
-  const err = new Error(msg)
-  err.name = name
+  if (typeof msg === 'string') {
+    msg = new Error(msg)
+  }
+
+  msg.name = name
 
   // clean stack
   // remove name, message, and first line of stack (which is this file)
-  err.stack = err.stack
-    .replace(err.name, '')
+  msg.stack = msg.stack
+    .replace(msg.name, '')
     .replace(error.message, '')
     .split('\n')
     .filter((_, i) => i !== 1)
     .join('\n')
 
-  return err
+  return msg
 }
